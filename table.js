68ac22f55ee1cc04747e38d3f1e328d578ad2522
shoaibkhan73 <khan973973@gmail.com>
2024-12-06 01:50:42 +0500

    exercise 1 makking a table using docoment i am completed exercise 1

---
+++ table.js
@@ -0,0 +1,312 @@
+let tab = document.createElement("table");
+tab.setAttribute("border", "2");
+let h1 = document.createElement("h1");
+h1.innerText = " Mke Time Table";
+h1.style.textAlign = 'center';    
+tab.appendChild(h1);
+let tr1 = document.createElement("tr");
+let td1 = document.createElement("td");
+td1.innerText = "Days";
+td1.setAttribute("rowspan", "1");
+tr1.appendChild(td1);
+
+let td2 = document.createElement("td");
+td2.innerText = "2nd 9:20 to 10";
+td2.setAttribute("rowspan", "1");
+tr1.appendChild(td2);
+
+let td3 = document.createElement("td");
+td3.innerText = "3rd 10 to 10:40";
+td3.setAttribute("rowspan", "1");
+tr1.appendChild(td3);
+
+let td4 = document.createElement("td");
+td4.innerText = "4th 10:40 to 11:20";
+td4.setAttribute("rowspan", "1");
+tr1.appendChild(td4);
+
+let td5 = document.createElement("td");
+td5.innerText = "20 mint breake";
+td5.setAttribute("rowspan", "11");
+tr1.appendChild(td5);
+
+let td6 = document.createElement("td");
+td6.innerText = "5th 11:40 to 12:20";
+td6.setAttribute("rowspan", "1");
+tr1.appendChild(td6);
+
+let td7 = document.createElement("td");
+td7.innerText = "6th  12:20 to 1pm";
+td7.setAttribute("rowspan", "1");
+tr1.appendChild(td7);
+
+let td8 = document.createElement("td");
+td8.innerText = "7th  1pm to 1:40pm";
+td8.setAttribute("rowspan", "1");
+tr1.appendChild(td8);
+
+let td9 = document.createElement("td");
+td9.innerText = "8th 1:40pm to 2:20pm";
+td9.setAttribute("rowspan", "1");
+tr1.appendChild(td9);
+
+tab.appendChild(tr1);
+
+let tr2 = document.createElement("tr");
+
+let tr2td1 = document.createElement("td");
+tr2td1.innerText = "Saturday";
+tr2.appendChild(tr2td1);
+
+let tr2td2 = document.createElement("td");
+tr2td2.innerText = "English";
+tr2.appendChild(tr2td2);
+
+let tr2td3 = document.createElement("td");
+tr2td3.innerText = "Math";
+tr2.appendChild(tr2td3);
+
+let tr2td4 = document.createElement("td");
+tr2td4.innerText = "Comp-cs";
+tr2.appendChild(tr2td4);
+
+let tr2td5 = document.createElement("td");
+tr2td5.innerText = "Islami-st";
+tr2.appendChild(tr2td5);
+
+let tr2td7 = document.createElement("td");
+tr2td7.innerText = "Physics";
+tr2.appendChild(tr2td7);
+
+let tr2td8 = document.createElement("td");
+tr2td8.innerText = "Chemestry";
+tr2.appendChild(tr2td8);
+
+let tr2td9 = document.createElement("td");
+tr2td9.innerText = "urdu";
+tr2.appendChild(tr2td9);
+
+tab.appendChild(tr2);
+
+let tr3 = document.createElement("tr");
+
+let tr3td1 = document.createElement("td");
+tr3td1.innerText = "Sunday";
+tr3.appendChild(tr3td1);
+
+let tr3td2 = document.createElement("td");
+tr3td2.innerText = "11";
+tr3.appendChild(tr3td2);
+
+let tr3td3 = document.createElement("td");
+tr3td3.innerText = "11";
+tr3.appendChild(tr3td3);
+
+let tr3td4 = document.createElement("td");
+tr3td4.innerText = "11";
+tr3.appendChild(tr3td4);
+
+let tr3td6 = document.createElement("td");
+tr3td6.innerText = "11";
+tr3.appendChild(tr3td6);
+
+let tr3td7 = document.createElement("td");
+tr3td7.innerText = "11";
+tr3.appendChild(tr3td7);
+
+let tr3td8 = document.createElement("td");
+tr3td8.innerText = "11";
+tr3.appendChild(tr3td8);
+
+let tr3td9 = document.createElement("td");
+tr3td9.innerText = "11";
+tr3.appendChild(tr3td9);
+
+tab.appendChild(tr3);
+
+
+let tr4 = document.createElement("tr");
+
+let tr4td1 = document.createElement("td");
+tr4td1.innerText = "Monday";
+tr4.appendChild(tr4td1);
+
+let tr4td2 = document.createElement("td");
+tr4td2.innerText = "11";
+tr4.appendChild(tr4td2);
+
+let tr4td3 = document.createElement("td");
+tr4td3.innerText = "11";
+tr4.appendChild(tr4td3);
+
+let tr4td4 = document.createElement("td");
+tr4td4.innerText = "11";
+tr4.appendChild(tr4td4);
+
+let tr4td6 = document.createElement("td");
+tr4td6.innerText = "11";
+tr4.appendChild(tr4td6);
+
+let tr4td7 = document.createElement("td");
+tr4td7.innerText = "11";
+tr4.appendChild(tr4td7);
+
+let tr4td8 = document.createElement("td");
+tr4td8.innerText = "11";
+tr4.appendChild(tr4td8);
+
+let tr4td9 = document.createElement("td");
+tr4td9.innerText = "11";
+tr4.appendChild(tr4td9);
+
+tab.appendChild(tr4);
+
+let tr5 = document.createElement("tr");
+
+let tr5td1 = document.createElement("td");
+tr5td1.innerText = "Tuesday";
+tr5.appendChild(tr5td1);
+
+let tr5td2 = document.createElement("td");
+tr5td2.innerText = "11";
+tr5.appendChild(tr5td2);
+
+let tr5td3 = document.createElement("td");
+tr5td3.innerText = "11";
+tr5.appendChild(tr5td3);
+
+let tr5td4 = document.createElement("td");
+tr5td4.innerText = "11";
+tr5.appendChild(tr5td4);
+
+let tr5td6 = document.createElement("td");
+tr5td6.innerText = "11";
+tr5.appendChild(tr5td6);
+
+let tr5td7 = document.createElement("td");
+tr5td7.innerText = "11";
+tr5.appendChild(tr5td7);
+
+let tr5td8 = document.createElement("td");
+tr5td8.innerText = "11";
+tr5.appendChild(tr5td8);
+
+let tr5td9 = document.createElement("td");
+tr5td9.innerText = "11";
+tr5.appendChild(tr5td9);
+
+tab.appendChild(tr5);
+
+let tr6 = document.createElement("tr");
+
+let tr6td1 = document.createElement("td");
+tr6td1.innerText = "wednesday";
+tr6.appendChild(tr6td1);
+
+let tr6td2 = document.createElement("td");
+tr6td2.innerText = "11";
+tr6.appendChild(tr6td2);
+
+let tr6td3 = document.createElement("td");
+tr6td3.innerText = "11";
+tr6.appendChild(tr6td3);
+
+let tr6td4 = document.createElement("td");
+tr6td4.innerText = "11";
+tr6.appendChild(tr6td4);
+
+let tr6td6 = document.createElement("td");
+tr6td6.innerText = "11";
+tr6.appendChild(tr6td6);
+
+let tr6td7 = document.createElement("td");
+tr6td7.innerText = "11";
+tr6.appendChild(tr6td7);
+
+let tr6td8 = document.createElement("td");
+tr6td8.innerText = "11";
+tr6.appendChild(tr6td8);
+
+let tr6td9 = document.createElement("td");
+tr6td9.innerText = "11";
+tr6.appendChild(tr6td9);
+
+tab.appendChild(tr6);
+
+
+
+let tr7 = document.createElement("tr");
+
+let tr7td1 = document.createElement("td");
+tr7td1.innerText = "Thursday";
+tr7.appendChild(tr7td1);
+
+let tr7td2 = document.createElement("td");
+tr7td2.innerText = "11";
+tr7.appendChild(tr7td2);
+
+let tr7td3 = document.createElement("td");
+tr7td3.innerText = "11";
+tr7.appendChild(tr7td3);
+
+let tr7td4 = document.createElement("td");
+tr7td4.innerText = "11";
+tr7.appendChild(tr7td4);
+
+let tr7td6 = document.createElement("td");
+tr7td6.innerText = "11";
+tr7.appendChild(tr7td6);
+
+let tr7td7 = document.createElement("td");
+tr7td7.innerText = "11";
+tr7.appendChild(tr7td7);
+
+let tr7td8 = document.createElement("td");
+tr7td8.innerText = "11";
+tr7.appendChild(tr7td8);
+
+let tr7td9 = document.createElement("td");
+tr7td9.innerText = "11";
+tr7.appendChild(tr7td9);
+
+tab.appendChild(tr7);
+
+
+let tr8 = document.createElement("tr");
+
+let tr8td1 = document.createElement("td");
+tr8td1.innerText = "Friday";
+tr8.appendChild(tr8td1);
+
+let tr8td2 = document.createElement("td");
+tr8td2.innerText = "11";
+tr8.appendChild(tr8td2);
+
+let tr8td3 = document.createElement("td");
+tr8td3.innerText = "11";
+tr8.appendChild(tr8td3);
+
+let tr8td4 = document.createElement("td");
+tr8td4.innerText = "11";
+tr8.appendChild(tr8td4);
+
+let tr8td6 = document.createElement("td");
+tr8td6.innerText = "11";
+tr8.appendChild(tr8td6);
+
+let tr8td7 = document.createElement("td");
+tr8td7.innerText = "11";
+tr8.appendChild(tr8td7);
+
+let tr8td8 = document.createElement("td");
+tr8td8.innerText = "11";
+tr8.appendChild(tr8td8);
+
+let tr8td9 = document.createElement("td");
+tr8td9.innerText = "11";
+tr8.appendChild(tr8td9);
+
+tab.appendChild(tr8);
+
+
+document.body.appendChild(tab);
